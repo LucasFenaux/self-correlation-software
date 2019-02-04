6f1432bf153de62437b7378758cba0059e7de354
Lucas Fenaux <46611744+LucasFenaux@users.noreply.github.com>
2019-02-04 13:51:54 -0500

    Update Main.java
    Updated the loader to make the .jar executable work usable and removed useless imports

--- Controller/Main.java
+++ Controller/Main.java
@@ -1,22 +1,17 @@
 package Controller;
 
 
-import Model.SCS;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
 
 
 public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/SelectionPage.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/View/SelectionPage.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Self-correlation Software");
